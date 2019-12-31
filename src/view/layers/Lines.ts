d1d28e8200cfd7cda411ef0f43d469d7371106be
Alex Burner <alex.burner@gmail.com>
2019-12-30 16:44:03 -0800

    Switch lines to shader material

--- src/view/layers/Lines.ts
+++ src/view/layers/Lines.ts
@@ -5,31 +5,46 @@ import Particle3 from 'src/particles/Particle3'
 import { NeighborMsg } from 'src/particles/System'
 import { Layer, LayerArgs } from 'src/view/Layers'
 
+const POINT_COUNT = MAX_NEIGHBORS * 3
+const OPACITY = 0.6
+
 export default class Lines implements Layer {
-  private readonly group: THREE.Group
-  private readonly positions: Float32Array
-  private readonly posAttr: THREE.BufferAttribute
   private readonly geometry: THREE.BufferGeometry
   private readonly lineSegments: THREE.LineSegments
 
+  private readonly positions: Float32Array
+  private readonly posAttr: THREE.BufferAttribute
+
+  private readonly alphas: Float32Array
+  private readonly alphaAttr: THREE.BufferAttribute
+
   constructor(group: THREE.Group) {
-    this.group = group
-    this.positions = new Float32Array(MAX_NEIGHBORS * 3)
-    this.posAttr = new THREE.BufferAttribute(this.positions, 3).setDynamic(true)
     this.geometry = new THREE.BufferGeometry()
+
+    this.positions = new Float32Array(POINT_COUNT)
+    this.posAttr = new THREE.BufferAttribute(this.positions, 3).setDynamic(true)
     this.geometry.addAttribute('position', this.posAttr)
+
+    this.alphas = new Float32Array(POINT_COUNT * 2) // 2 for each segment's points
+    this.alphas.fill(OPACITY) // begin at default opacity
+    this.alphaAttr = new THREE.BufferAttribute(this.alphas, 1).setDynamic(true)
+    this.geometry.addAttribute('alphas', this.alphaAttr)
+
     this.geometry.computeBoundingSphere()
     this.geometry.setDrawRange(0, 0)
+
     this.lineSegments = new THREE.LineSegments(
       this.geometry,
-      new THREE.LineBasicMaterial({
-        blending: THREE.AdditiveBlending,
+      new THREE.ShaderMaterial({
+        uniforms: { color: { value: new THREE.Color(0xffffff) } },
+        vertexShader,
+        fragmentShader,
         transparent: true,
-        color: 0xffffff,
-        opacity: 0.6,
+        blending: THREE.AdditiveBlending,
+        depthTest: false,
       }),
     )
-    this.group.add(this.lineSegments)
+    group.add(this.lineSegments)
   }
 
   public update({ particles, neighborhood }: LayerArgs): void {
@@ -48,11 +63,41 @@ export default class Lines implements Layer {
       })
     })
 
-    this.geometry.setDrawRange(0, numConnected * 2)
+    const drawRange = numConnected * 2
+    this.geometry.setDrawRange(0, drawRange)
+
+    if (neighborhood.type === 'proximity') {
+      // set each line to fade based on length
+      // TODO
+    } else {
+      // set all lines to default opacity
+      this.alphas.fill(OPACITY, 0, drawRange)
+    }
+
     this.posAttr.needsUpdate = true
+    this.alphaAttr.needsUpdate = true
   }
 
   public clear(): void {
     this.geometry.setDrawRange(0, 0)
   }
 }
+
+const vertexShader = `
+  attribute float alpha;
+  varying float vAlpha;
+
+  void main() {
+    vAlpha = alpha;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  }
+`
+
+const fragmentShader = `
+  uniform vec3 color;
+  varying float vAlpha;
+
+  void main() {
+    gl_FragColor = vec4(color, 1.0);
+  }
+`
